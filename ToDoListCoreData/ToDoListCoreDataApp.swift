152582d2e345835efb66d8eee00f2fec85cc1888
Marwan400 <marwan40059@gmail.com>
2024-04-12 04:46:00 +0300

    Initial Commit

---
+++ ToDoListCoreData/ToDoListCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListCoreDataApp.swift
+//  ToDoListCoreData
+//
+//  Created by Marwan Al.Jabri on 03/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
